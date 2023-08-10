363ec62d85b2638a42d85282c76d78ef87d4cdd1
JovemDevv <anacarolinesan18@gmail.com>
2023-08-09 22:40:35 -0300

    configurações iniciais do servidor

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require("cors")
+const {server: api} = require("./src/api/index")
+
+const app = express()
+const port = process.env.port || 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(cors())
+app.use(express.json())
+app.use("/api", api)
+
+app.listen(port, () => {
+  console.log(`O servidor está funcionando na porta: ${port}`)
+})
